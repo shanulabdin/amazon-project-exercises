253ae3e1feec2d59f24645c5ff3fd3a797719153
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-07-20 06:35:05 +0500

    Fixed price foramting radio selectors

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 let productsHTML = '';
 
@@ -17,14 +18,14 @@ products.forEach((product) => {
 
       <div class="product-rating-container">
         <img class="product-rating-stars"
-          src="images/ratings/rating-${(product.rating.stars * 10).toFixed()}.png">
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
         <div class="product-rating-count link-primary">
           ${product.rating.count}
         </div>
       </div>
 
       <div class="product-price">
-        ${product.priceCents / 100}
+        $${formatCurrency(product.priceCents)}
       </div>
 
       <div class="product-quantity-container">
