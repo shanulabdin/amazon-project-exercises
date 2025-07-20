637ce7ac6c05e9025c133a1b26449614d5e4fae3
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-07-21 00:55:40 +0500

    Modified Update button

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -32,9 +32,11 @@ cart.forEach((cartItem) => {
             <span>
               Quantity: <span class="quantity-label">${cartItem.quantity}</span>
             </span>
-            <span class="update-quantity-link link-primary">
+            <span class="update-quantity-link link-primary js-update-link" data-product-id="${matchingProduct.id}">
               Update
             </span>
+            <input class="quantity-input">
+            <span class="save-quantity-link link-primary">Save</span>
             <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
               Delete
             </span>
@@ -110,3 +112,19 @@ function updateCartQuantity() {
     .innerHTML = `${cartQuantity} items`;
 }
 updateCartQuantity();
+
+
+document.querySelectorAll(`.js-update-link`)
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+      const productId = link.dataset.productId;
+
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+
+      container.classList.add('is-editing-quantity'); 
+  });
+  
+})
+  
