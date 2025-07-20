1bc4be3e0a1f6d809f6e177d7f41c19655bc031c
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-07-21 03:39:54 +0500

    Doing all the exercises by heart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,9 +1,9 @@
-import {cart, addToCart,
-  calculateCartQuantity} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
-import {formatCurrency} from './utils/money.js';
+import { formatCurrency } from './utils/money.js';
 
 let productsHTML = '';
+
 products.forEach((product) => {
   productsHTML += `
     <div class="product-container">
@@ -11,9 +11,11 @@ products.forEach((product) => {
         <img class="product-image"
           src="${product.image}">
       </div>
+
       <div class="product-name limit-text-to-2-lines">
         ${product.name}
       </div>
+
       <div class="product-rating-container">
         <img class="product-rating-stars"
           src="images/ratings/rating-${product.rating.stars * 10}.png">
@@ -21,11 +23,13 @@ products.forEach((product) => {
           ${product.rating.count}
         </div>
       </div>
+
       <div class="product-price">
         $${formatCurrency(product.priceCents)}
       </div>
+
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -38,33 +42,45 @@ products.forEach((product) => {
           <option value="10">10</option>
         </select>
       </div>
+
       <div class="product-spacer"></div>
-      <div class="added-to-cart">
+
+      <div class="added-to-cart js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
       </div>
+
       <button class="add-to-cart-button button-primary js-add-to-cart"
       data-product-id="${product.id}">
         Add to Cart
       </button>
     </div>
   `;
+
+  
 });
-document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-function updateCartQuantity() {
 
-  const cartQuantity = calculateCartQuantity();
+document.querySelector('.js-products-grid')
+  .innerHTML = productsHTML;
+
+
+function UpdateCartQuantity(){
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
 
   document.querySelector('.js-cart-quantity')
     .innerHTML = cartQuantity;
 }
-updateCartQuantity();
-document.querySelectorAll('.js-add-to-cart')
+
+document.querySelectorAll('.js-add-to-cart') 
   .forEach((button) => {
-    button.addEventListener('click', () => {
-      const productId = button.dataset.productId;
+    button.addEventListener('click', () =>{
+      const {productId} = button.dataset;
       addToCart(productId);
-      updateCartQuantity();
-    });
+      UpdateCartQuantity();
+    })
   });
