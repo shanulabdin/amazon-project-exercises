c3ea8b016f5f2abeaf25c250d5b9c0ec790eb914
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-07-21 06:47:19 +0500

    14j. clicking save makes the input element disappear

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -47,7 +47,7 @@ cart.forEach((cartItem) => {
               Update
             </span>
             <input class="quantity-input">
-            <span class="save-quantity-link link-primary">
+            <span class="save-quantity-link link-primary js-save-primary" data-product-id="${matchingProduct.id}">
               Save
             </span>
               <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
@@ -140,6 +140,16 @@ document.querySelectorAll('.js-update-link')
       const container = document.querySelector(`.js-cart-item-container-${productId}`);
       container.classList.add('is-editing-quantity');
 
+
     })
   });
 
+document.querySelectorAll('.js-save-primary')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+      const productId = link.dataset.productId;
+
+      const container = document.querySelector(`.js-cart-item-container-${productId}`);
+      container.classList.remove('is-editing-quantity');
+    })
+  })
