cb1a5af212b79c7388078012a800fb33daecc7c4
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-08-06 01:34:45 +0500

    trying a, b, c exercise by heart, my way

--- lesson 15 exercises/data/car.js
+++ lesson 15 exercises/data/car.js
@@ -1,6 +1,7 @@
 class Car {
   brand;
   model;
+  speed = 0;
 
   constructor(carDetails){
     this.brand = carDetails.brand;
@@ -8,22 +9,68 @@ class Car {
   }
 
   displayInfo(){
-    console.log(`${this.brand} ${this.model}`)
+    const carInfo = `${this.brand} ${this.model}`;
+    const speedInfo = `${this.speed}`
+
+    console.log(carInfo, '- Speed:', speedInfo)
+  }
+
+  go(){
+    this.speed += 5;
+
+    if(this.speed > 200){
+      this.speed = 200;
+    }
+  }
+
+  brake(){
+    this.speed -= 5;
+
+    if(this.speed < 0){
+      this.speed = 0
+    }
   }
 }
 
 const car1 = new Car ({
   brand: 'Toyota',
-  model: 'Corola'
+  model: 'Corola',
 })
 
 const car2 = new Car ({
   brand: 'Tesla',
-  model: 'model 3'
+  model: 'Model 3'
 })
 
-console.log(car1);
-console.log(car2);
+console.log(car1)
+car1.go()
+car1.displayInfo()
+car1.go()
 
-car1.displayInfo();
-car2.displayInfo();
+car1.brake()
+car1.brake()
+car1.go()
+car1.go()
+car1.go()
+car1.brake()
+car1.brake()
+car1.brake()
+car1.brake()
+car1.displayInfo()
+
+console.log(car2)
+car2.go()
+car2.displayInfo()
+car2.go()
+
+car2.brake()
+car2.brake()
+car2.go()
+car2.go()
+car2.go()
+car2.brake()
+car2.brake()
+car2.brake()
+car2.brake()
+
+car2.displayInfo()
