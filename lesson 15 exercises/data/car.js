5a14d9ba12622bfe35814db72992f28a147705c8
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-08-07 07:14:01 +0500

    extented the class by adding racecar

--- lesson 15 exercises/data/car.js
+++ lesson 15 exercises/data/car.js
@@ -46,6 +46,32 @@ class Car {
   }
 }
 
+class RaceCar extends Car {
+  acceleration;
+
+  constructor(carDetails){
+    super(carDetails);
+    this.acceleration = carDetails.acceleration;
+  }
+
+  go(){
+    this.speed += this.acceleration;
+
+    if(this.speed > 300){
+      this.speed = 300;
+    }
+  }
+
+  openTrunk(){
+    console.log('Race cars do not have a trunk')
+  }
+
+  closeTrunk(){
+    console.log('Race cars do not have a trunk')
+  }
+
+}
+
 const car1 = new Car ({
   brand: 'Toyota',
   model: 'Corola',
@@ -56,45 +82,65 @@ const car2 = new Car ({
   model: 'Model 3'
 })
 
+const raceCar = new RaceCar ({
+  brand: 'Mclaren',
+  model: 'F1',
+  acceleration: 20
+}) 
 
-console.log(car1)
-car1.displayInfo()
+// new tests
+console.log(raceCar)
+raceCar.displayInfo()
 
-car1.openTrunk()
-car1.displayInfo()
+raceCar.go()
+raceCar.displayInfo()
 
-car1.go()
-car1.displayInfo()
-
-car1.closeTrunk()
-car1.displayInfo()
-
-car1.go()
-car1.openTrunk()
-car1.displayInfo()
-
-car1.brake()
-car1.openTrunk()
-console.log(car1)
-
+raceCar.openTrunk()
+raceCar.closeTrunk()
+raceCar.go()
+raceCar.go()
+raceCar.go()
+raceCar.displayInfo()
 
 
-console.log(car2)
-car2.displayInfo()
+// console.log(car1)
+// car1.displayInfo()
 
-car2.openTrunk()
-car2.displayInfo()
+// car1.openTrunk()
+// car1.displayInfo()
 
-car2.go()
-car2.displayInfo()
+// car1.go()
+// car1.displayInfo()
 
-car2.closeTrunk()
-car2.displayInfo()
+// car1.closeTrunk()
+// car1.displayInfo()
 
-car2.go()
-car2.openTrunk()
-car2.displayInfo()
+// car1.go()
+// car1.openTrunk()
+// car1.displayInfo()
 
-car2.brake()
-car2.openTrunk()
-console.log(car2)
+// car1.brake()
+// car1.openTrunk()
+// console.log(car1)
+
+
+
+// console.log(car2)
+// car2.displayInfo()
+
+// car2.openTrunk()
+// car2.displayInfo()
+
+// car2.go()
+// car2.displayInfo()
+
+// car2.closeTrunk()
+// car2.displayInfo()
+
+// car2.go()
+// car2.openTrunk()
+// car2.displayInfo()
+
+// car2.brake()
+// car2.openTrunk()
+// console.log(car2)
