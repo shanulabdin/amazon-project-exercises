93a34e5752788960cf2ef4899ce759a1cddcafd5
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-08-07 05:23:23 +0500

    17d, practice

--- lesson 15 exercises/data/car.js
+++ lesson 15 exercises/data/car.js
@@ -2,6 +2,7 @@ class Car {
   brand;
   model;
   speed = 0;
+  isTrunkOpen = false;
 
   constructor(carDetails){
     this.brand = carDetails.brand;
@@ -10,13 +11,16 @@ class Car {
 
   displayInfo(){
     const carInfo = `${this.brand} ${this.model}`;
-    const speedInfo = `${this.speed}`
+    const speedInfo = `${this.speed}`;
+    const trunkStatus = this.isTrunkOpen ? 'open' : 'closed';
 
-    console.log(carInfo, '- Speed:', speedInfo)
+    console.log(carInfo, '- Speed:', speedInfo, 'km/h -', 'trunk:', trunkStatus)
   }
 
   go(){
-    this.speed += 5;
+    if(!this.isTrunkOpen){
+      this.speed += 5;
+    }
 
     if(this.speed > 200){
       this.speed = 200;
@@ -30,6 +34,16 @@ class Car {
       this.speed = 0
     }
   }
+
+  openTrunk(){
+      if(this.speed === 0){
+      this.isTrunkOpen = true
+    }
+  }
+
+  closeTrunk(){
+    this.isTrunkOpen = false
+  }
 }
 
 const car1 = new Car ({
@@ -42,35 +56,34 @@ const car2 = new Car ({
   model: 'Model 3'
 })
 
+// my tests
 console.log(car1)
+
+car1.openTrunk()
+car1.displayInfo()
+
+car1.closeTrunk()
+car1.displayInfo()
+
 car1.go()
 car1.displayInfo()
-car1.go()
 
-car1.brake()
-car1.brake()
 car1.go()
-car1.go()
-car1.go()
-car1.brake()
-car1.brake()
-car1.brake()
-car1.brake()
+car1.openTrunk()
 car1.displayInfo()
 
 console.log(car2)
+
+car2.openTrunk()
+car2.displayInfo()
+
+car2.openTrunk()
 car2.go()
 car2.displayInfo()
-car2.go()
 
-car2.brake()
-car2.brake()
-car2.go()
-car2.go()
-car2.go()
-car2.brake()
-car2.brake()
-car2.brake()
-car2.brake()
+car2.closeTrunk()
+car2.displayInfo()
 
+car2.go()
+car2.openTrunk()
 car2.displayInfo()
